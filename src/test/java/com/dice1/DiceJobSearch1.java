5fc6ce54966fdd8b190214d38d24824f3221fdb7
Kamran.MB <kmb7855@gmail.com>
2018-11-26 20:50:54 -0600

    first project

---
+++ src/test/java/com/dice1/DiceJobSearch1.java
@@ -0,0 +1,59 @@
+package com.dice1;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class DiceJobSearch1 {
+	public static void main(String[] args)throws Exception  {
+				
+				WebDriverManager.chromedriver().setup();
+				WebDriver dr=new ChromeDriver();
+				dr.manage().window().maximize();
+				dr.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
+				
+				
+				dr.get("https://www.dice.com/");
+				String key="SDET DEVELOPER";
+				String act=dr.getTitle();
+				String exp="Find Jobs in Tech | Dice.com";
+				
+				
+				if (act.equals(exp)) {
+					System.out.println("pass");
+				}else {
+					System.out.println("fail");
+					throw new Exception("check again");
+				}
+				
+				dr.findElement(By.id("search-field-keyword")).clear();
+				dr.findElement(By.id("search-field-keyword")).sendKeys(key);
+				String location="77478";
+				dr.findElement(By.id("search-field-location")).clear();
+				dr.findElement(By.id("search-field-location")).sendKeys(location+Keys.ENTER);
+				String count=dr.findElement(By.id("posiCountMobileId")).getText();
+				System.out.println(count);
+				
+				int cr=Integer.parseInt(count);
+							
+						if (cr>0) {
+					System.out.println("Step pass. key :" +key +" search returned "+ cr +" result in " +location);
+				}else {
+					System.out.println("\"Step fail:  key :\" +key +\" search returned \"+ countR +\"result in \" +location..");
+				}
+
+				
+					
+				}
+}
+				
+			
+
+
+
+
+	
